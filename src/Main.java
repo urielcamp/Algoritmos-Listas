4135fa666f142d20753f230204de09ccf2f3231f
Uriel Campaña <urielcamp@gmail.com>
2024-10-08 10:55:08 -0300

    segundo ejercicio

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -5,18 +7,32 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args){
 
+        //EJERCICIO 1
+
         LinkedList<String>tareasPendientes = new LinkedList<>();
 
         tareasPendientes.add("barrer");
         tareasPendientes.add("limpiar");
         tareasPendientes.add("ordenar");
 
-         System.out.println(tareasPendientes.toString());
+         //System.out.println(tareasPendientes.toString());
 
          tareasPendientes.remove("limpiar");
-        System.out.println(tareasPendientes.toString());
+        //System.out.println(tareasPendientes.toString());
 
+        //EJERCICIO 2
 
+        LinkedList<Integer>numeros = new LinkedList<Integer>();
+
+        numeros.add(3);
+        numeros.add(2);
+        numeros.add(5);
+        numeros.add(9);
+        numeros.add(45);
+        numeros.add(32);
+
+        Collections.sort(numeros);
+        System.out.println(numeros.toString());
 
     }
 }
